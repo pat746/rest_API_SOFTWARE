588b54836294f5bae4f09d22d82cf08f8ca05e61
Patricia Ortiz Salvador <patysalva_99@hotmail.com>
2025-05-09 16:33:47 -0500

    Subir los archivos correctamente

---
+++ src/routes/softwares.routes.js
@@ -0,0 +1,22 @@
+import {Router} from 'express'
+import { 
+  createSoftwares, 
+  deleteSoftwares, 
+  getSoftwareById, 
+  getSoftwares, 
+  updateSoftwares 
+} from '../controllers/softwares.controller.js'
+
+const router = Router()
+
+//REST API = verbos
+router.get('/softwares', getSoftwares)
+router.get('/softwares/:id', getSoftwareById)
+
+router.post('/softwares', createSoftwares)
+
+router.put('/softwares/:id', updateSoftwares)
+
+router.delete('/softwares/:id', deleteSoftwares)
+
+export default router
